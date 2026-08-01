6b63f3f9cabde109761a943b340a8226e127c930
Kim Ji Hyeon <117696182+Hyeoniss@users.noreply.github.com>
2026-08-01 20:12:33 +0900

    feat: 구글 로그인 및 게스트 로그인 구현

---
+++ src/main/java/hajiton/chaebee/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,9 @@
+package hajiton.chaebee.apiPayload.code;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+    HttpStatus getStatus();
+    String getCode();
+    String getMessage();
+}
